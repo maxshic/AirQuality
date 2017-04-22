f1cd98e6de1ea3226c6f2705e2eafa6d5c842887
最爱滕小厨 <teng520jiao@qq.com>
2017-04-22 19:46:26 +0800

    menu

--- app/src/main/java/cn/teng520/airquality/MainActivity.java
+++ app/src/main/java/cn/teng520/airquality/MainActivity.java
@@ -1,13 +1,20 @@
 package cn.teng520.airquality;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return true;
     }
 }
