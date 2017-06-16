1f603b9b774317fdea761ef2fc07906e6db17130
最爱滕小厨 <teng520jiao@qq.com>
2017-06-16 16:46:11 +0800

    menu

--- app/src/main/java/cn/teng520/airquality/MainActivity.java
+++ app/src/main/java/cn/teng520/airquality/MainActivity.java
@@ -2,10 +2,18 @@ package cn.teng520.airquality;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 import java.io.IOException;
@@ -13,14 +21,42 @@ import java.io.IOException;
 public class MainActivity extends Activity {
 
     private ListView airList;
-    private String city2 = "changshu";
+    private String city2;
+    private String[] city_name;
+    private String[] city_code;
+    private ArrayAdapter<String> adapter;
+    private TextView area;
+    private TextView aqi;
+    private TextView quality;
+    private TextView primary_pollutant;
+    private TextView time_point;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        city_name = getResources().getStringArray(R.array.city1);
+        city_code = getResources().getStringArray(R.array.city2);
+        adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,city_name);
         airList = (ListView)findViewById(R.id.airList);
+        area = (TextView)findViewById(R.id.area);
+        aqi = (TextView)findViewById(R.id.aqi);
+        quality = (TextView)findViewById(R.id.quality);
+        primary_pollutant = (TextView)findViewById(R.id.primary_pollutant);
+        time_point = (TextView)findViewById(R.id.time_point);
+        imageView = (ImageView)findViewById(R.id.button_refresh);
+
+        SharedPreferences sp = getSharedPreferences("aqi", Context.MODE_PRIVATE);
+        city2 = sp.getString("code","changshu");
         loadAQI(city2);
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chooseCity();
+            }
+        });
     }
 
     private void loadAQI(final String city2){
@@ -43,8 +79,18 @@ public class MainActivity extends Activity {
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                //cityTextView.setText(weather.getCity());
-                MyAdapter adapter = new MyAdapter(MainActivity.this,aqis);
+                int n = aqis.length;
+                AQI[] temps = new AQI[n-1];
+                for(int i=0;i<temps.length;i++){
+                    temps[i] = aqis[i];
+                }
+                AQI temp = aqis[n-1];
+                area.setText(temp.getArea());
+                aqi.setText(temp.getAqi());
+                quality.setText(String.format(getResources().getString(R.string.quality),temp.getQuality()));
+                primary_pollutant.setText(String.format(getResources().getString(R.string.primary_pollutant),temp.getPrimary_pollutant()));
+                time_point.setText(String.format(getResources().getString(R.string.time_point),temp.getTime_point()));
+                MyAdapter adapter = new MyAdapter(MainActivity.this,temps);
                 airList.setAdapter(adapter);
             }
         });
@@ -60,12 +106,30 @@ public class MainActivity extends Activity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch(item.getItemId()){
             case R.id.city_choose:
+                chooseCity();
                 return true;
             case R.id.about:
                 showAbout();
                 return true;
         }
         return false;
+    }
+
+    private void chooseCity() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("选择城市:");
+        builder.setAdapter(adapter, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                SharedPreferences sp = getSharedPreferences("aqi", Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sp.edit();
+                editor.putString("code",city_code[i]);
+                editor.commit();
+                Toast.makeText(MainActivity.this,city_name[i],Toast.LENGTH_SHORT).show();
+                loadAQI(city_code[i]);
+            }
+        });
+        builder.show();
     }
 
     private void showAbout() {
